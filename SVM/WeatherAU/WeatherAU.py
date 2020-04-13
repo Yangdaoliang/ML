bf4fdf701b1e82f80bcca480a8331bad691b67ad
yangdaoliang <1524871043@qq.com>
2020-04-13 08:26:53 +0800

    Kaggle WeatherAU

---
+++ SVM/WeatherAU/WeatherAU.py
@@ -0,0 +1,316 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+#读取数据
+weatherData = pd.read_csv('../data/weatherAUS5000.csv', index_col=0)
+#将特征矩阵和标签分开
+X = weatherData.iloc[:, :-1]
+Y = weatherData.iloc[:, -1]
+
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.3, random_state=30)
+for i in [Xtrain, Xtest, Ytrain, Ytest]:
+    i.index = range(0, i.shape[0])
+
+#将标签编码
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder().fit(Ytrain)
+
+Ytrain = pd.DataFrame(encoder.transform(Ytrain))
+Ytest = pd.DataFrame(encoder.transform(Ytest))
+
+#描述性统计
+Xtrain.describe([0.01,0.05,0.1,0.25,0.5,0.75,0.9,0.99]).T
+
+Xtrainc = Xtrain.copy()
+#构建新特征
+Xtrain.loc[Xtrain['Rainfall'] >= 1, 'RainToday'] = 'Yes'
+Xtrain.loc[Xtrain['Rainfall'] < 1, 'RainToday'] = 'No'
+Xtrain.loc[Xtrain['Rainfall'] == np.nan, 'RainToday'] = np.nan
+
+Xtest.loc[Xtest['Rainfall'] >= 1,'RainToday'] = 'Yes'
+Xtest.loc[Xtest['Rainfall'] < 1,'RainToday'] = 'No'
+Xtest.loc[Xtest['Rainfall'] == np.nan,'RainToday'] = np.nan
+
+Xtrain['Date'] = Xtrain['Date'].apply(lambda x:int(x.split('-')[1]))
+
+Xtest['Date'] = Xtest['Date'].apply(lambda x:int(x.split('-')[1]))
+
+Xtrain = Xtrain.rename(columns={'Date':'Month'})
+Xtest = Xtest.rename(columns={'Date':'Month'})
+
+cityll = pd.read_csv('../data/cityll.csv', index_col = 0)
+city_climate = pd.read_csv('../data/Cityclimate.csv')
+#去掉度数符号
+cityll["Latitudenum"] = cityll["Latitude"].apply(lambda x: float(x[:-1]))
+cityll["Longitudenum"] = cityll["Longitude"].apply(lambda x: float(x[:-1]))
+
+
+#观察一下所有的经纬度方向都是一致的，全部是南纬，东经，因为澳大利亚在南半球，东半球
+#所以经纬度的方向我们可以舍弃了
+citylld = cityll.iloc[:, [0, 5, 6]]
+#将city_climate中的气候添加到我们的citylld中
+citylld["climate"] = city_climate.iloc[:, -1]
+city_climate['Climate'].head()
+
+samplecity = pd.read_csv('../data/samplecity.csv',index_col = 0)
+samplecity.head()
+
+#我们对samplecity也执行同样的处理：去掉经纬度中度数的符号，并且舍弃我们的经纬度的方向
+samplecity["Latitudenum"] = samplecity["Latitude"].apply(lambda x:float(x[:-1]))
+samplecity["Longitudenum"] = samplecity["Longitude"].apply(lambda x:float(x[:-1]))
+samplecityd = samplecity.iloc[:,[0,5,6]]
+samplecityd.head()
+
+
+#首先使用radians将角度转换成弧度
+from math import radians
+citylld.loc[:,"slat"] = citylld.iloc[:,1].apply(lambda x : radians(x))
+citylld.loc[:,"slon"] = citylld.iloc[:,2].apply(lambda x : radians(x))
+samplecityd.loc[:, "elat"] = samplecityd.iloc[:,1].apply(lambda x : radians(x))
+samplecityd.loc[:, "elon"] = samplecityd.iloc[:,2].apply(lambda x : radians(x))
+
+for i in range(samplecityd.shape[0]):
+    slat = citylld.loc[:,"slat"]
+    slon = citylld.loc[:,"slon"]
+    elat = samplecityd.loc[i,"elat"]
+    elon = samplecityd.loc[i,"elon"]
+    dist = 6371.01 * np.arccos(np.sin(slat)*np.sin(elat) +
+                          np.cos(slat)*np.cos(elat)*np.cos(slon.values - elon))
+    city_index = np.argsort(dist)[0]
+    #每次计算后，取距离最近的城市，然后将最近的城市和城市对应的气候都匹配到samplecityd中
+    samplecityd.loc[i,"closest_city"] = citylld.loc[city_index,"City"]
+    samplecityd.loc[i,"climate"] = citylld.loc[city_index,"climate"]
+
+#确认无误后，取出样本城市所对应的气候，并保存
+locafinal = samplecityd.iloc[:,[0,-1]]
+locafinal.columns = ['Location', 'Climate']
+locafinal.head()
+
+locafinal = locafinal.set_index(keys='Location')
+
+Xtrain['Location'] = Xtrain['Location'].map(locafinal.iloc[:,0])
+
+#城市的气候中所含的逗号和空格都去掉
+import re
+Xtrain["Location"] = Xtrain["Location"].apply(lambda x:re.sub(",","",x.strip()))
+#城市的气候中所含的逗号和空格都去掉
+Xtest["Location"] = Xtest["Location"].map(locafinal.iloc[:,0]).apply(lambda x:re.sub(",","",x.strip()))
+
+#修改特征内容之后，我们使用新列名“Climate”来替换之前的列名“Location”
+#注意这个命令一旦执行之后，就再没有列"Location"了，使用索引时要特别注意
+Xtrain = Xtrain.rename(columns={"Location":"Climate"})
+Xtest = Xtest.rename(columns={"Location":"Climate"})
+Xtrain.dtypes == "object"
+#首先找出，分类型特征都有哪些
+cate = Xtrain.columns[Xtrain.dtypes == "object"].tolist()
+
+#除了特征类型为"object"的特征们，还有虽然用数字表示，但是本质为分类型特征的云层遮蔽程度
+cloud = ["Cloud9am","Cloud3pm"]
+cate = cate + cloud
+
+from sklearn.impute import SimpleImputer
+si = SimpleImputer(missing_values=np.nan,strategy="most_frequent")
+si.fit(Xtrain.loc[:,cate])
+
+Xtrain.isnull().mean()
+#然后我们用训练集中的众数来同时填补训练集和测试集
+Xtrain.loc[:,cate] = si.transform(Xtrain.loc[:,cate])
+Xtest.loc[:,cate] = si.transform(Xtest.loc[:,cate])
+
+#将所有的分类型变量编码为数字，一个类别是一个数字
+from sklearn.preprocessing import OrdinalEncoder #只允许二维以上的数据进行输入
+oe = OrdinalEncoder()
+#cate.remove('Month')
+#cate.remove('Climate')
+
+#利用训练集进行fit
+oe = oe.fit(Xtrain.loc[:, cate])
+
+#用训练集的编码结果来编码训练和测试特征矩阵
+#在这里如果测试特征矩阵报错，就说明测试集中出现了训练集中从未见过的类别
+Xtrain.loc[:,cate] = oe.transform(Xtrain.loc[:,cate])
+
+Xtest.loc[:,cate] = oe.transform(Xtest.loc[:,cate])
+
+col = Xtrain.columns.tolist()
+for i in cate:
+    col.remove(i)
+#实例化模型，填补策略为"mean"表示均值
+impmean = SimpleImputer(missing_values=np.nan,strategy = "mean")
+print(col)
+#用训练集来fit模型
+impmean = impmean.fit(Xtrain.loc[:, col])
+#分别在训练集和测试集上进行均值填补
+Xtrain.loc[:,col] = impmean.transform(Xtrain.loc[:,col])
+Xtest.loc[:,col] = impmean.transform(Xtest.loc[:,col])
+
+col.remove("Month")
+
+from sklearn.preprocessing import StandardScaler
+#数据转换为均值为0，方差为1的数据
+#标准化不改变数据的分布，不会把数据变成正态分布的
+
+ss = StandardScaler()
+ss = ss.fit(Xtrain.loc[:,col])
+Xtrain.loc[:,col] = ss.transform(Xtrain.loc[:,col])
+Xtest.loc[:,col] = ss.transform(Xtest.loc[:,col])
+
+
+from time import time
+#随时监控我们的模型的运行时间
+import datetime
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_auc_score, recall_score
+
+Ytrain = Ytrain.iloc[:,0].ravel()
+Ytest = Ytest.iloc[:,0].ravel()
+
+#建模选择自然是我们的支持向量机SVC，首先用核函数的学习曲线来选择核函数
+#我们希望同时观察，精确性，recall以及AUC分数
+times = time() #因为SVM是计算量很大的模型，所以我们需要时刻监控我们的模型运行时间
+
+for kernel in ["linear","poly","rbf","sigmoid"]:
+    clf = SVC(kernel = kernel
+              ,gamma="auto"
+              ,degree = 1
+              ,cache_size = 5000
+             ).fit(Xtrain, Ytrain)
+    result = clf.predict(Xtest)
+    score = clf.score(Xtest,Ytest)
+    recall = recall_score(Ytest, result)
+    auc = roc_auc_score(Ytest,clf.decision_function(Xtest))
+    print("%s 's testing accuracy %f, recall is %f', auc is %f" % (kernel,score,recall,auc))
+    print(datetime.datetime.fromtimestamp(time()-times).strftime("%M:%S:%f"))
+
+times = time()
+for kernel in ["linear","poly","rbf","sigmoid"]:
+    clf = SVC(kernel = kernel
+              ,gamma="auto"
+              ,degree = 1
+              ,cache_size = 5000
+              ,class_weight = "balanced"
+             ).fit(Xtrain, Ytrain)
+    result = clf.predict(Xtest)
+    score = clf.score(Xtest,Ytest)
+    recall = recall_score(Ytest, result)
+    auc = roc_auc_score(Ytest,clf.decision_function(Xtest))
+    print("%s 's testing accuracy %f, recall is %f', auc is %f" % (kernel,score,recall,auc))
+    print(datetime.datetime.fromtimestamp(time()-times).strftime("%M:%S:%f"))
+
+times = time()
+clf = SVC(kernel = "linear"
+          ,gamma="auto"
+          ,cache_size = 5000
+          ,class_weight = {1:15} #注意，这里写的其实是，类别1：10，隐藏了类别0：1这个比例
+         ).fit(Xtrain, Ytrain)
+result = clf.predict(Xtest)
+score = clf.score(Xtest,Ytest)
+recall = recall_score(Ytest, result)
+auc = roc_auc_score(Ytest,clf.decision_function(Xtest))
+print("testing accuracy %f, recall is %f', auc is %f" %(score,recall,auc))
+print(datetime.datetime.fromtimestamp(time()-times).strftime("%M:%S:%f"))
+
+valuec = pd.Series(Ytest).value_counts()
+
+#查看模型的特异度
+from sklearn.metrics import confusion_matrix as CM
+
+cm = CM(Ytest,result,labels=(1,0))
+
+irange = np.linspace(0.01, 0.05, 10)
+for i in irange:
+    times = time()
+    clf = SVC(kernel = "linear"
+              ,gamma="auto"
+              ,cache_size = 5000
+              ,class_weight = {1:1+i}
+             ).fit(Xtrain, Ytrain)
+    result = clf.predict(Xtest)
+    score = clf.score(Xtest,Ytest)
+    recall = recall_score(Ytest, result)
+    auc = roc_auc_score(Ytest,clf.decision_function(Xtest))
+    print("under ratio 1:%f testing accuracy %f, recall is %f', auc is %f" %(1+i,score,recall,auc))
+    print(datetime.datetime.fromtimestamp(time()-times).strftime("%M:%S:%f"))
+
+irange_ = np.linspace(0.018889,0.027778,10)
+
+for i in irange_:
+    times = time()
+    clf = SVC(kernel = "linear"
+              ,gamma="auto"
+              ,cache_size = 5000
+              ,class_weight = {1:1+i}
+             ).fit(Xtrain, Ytrain)
+    result = clf.predict(Xtest)
+    score = clf.score(Xtest,Ytest)
+    recall = recall_score(Ytest, result)
+    auc = roc_auc_score(Ytest,clf.decision_function(Xtest))
+    print("under ratio 1:%f testing accuracy %f, recall is %f', auc is %f" %(1+i,score,recall,auc))
+    print(datetime.datetime.fromtimestamp(time()-times).strftime("%M:%S:%f"))
+
+
+from sklearn.linear_model import LogisticRegression as LR
+
+logclf = LR(solver="liblinear").fit(Xtrain, Ytrain)
+logclf.score(Xtest,Ytest)
+
+C_range = np.linspace(5,10,10)
+
+for C in C_range:
+    logclf = LR(solver="liblinear",C=C).fit(Xtrain, Ytrain)
+    print(C,logclf.score(Xtest,Ytest))
+
+times = time()
+clf = SVC(kernel = "linear",C=3.1663157894736838,cache_size = 5000
+          ,class_weight = "balanced"
+         ).fit(Xtrain, Ytrain)
+result = clf.predict(Xtest)
+score = clf.score(Xtest,Ytest)
+recall = recall_score(Ytest, result)
+auc = roc_auc_score(Ytest,clf.decision_function(Xtest))
+print("testing accuracy %f,recall is %f', auc is %f" % (score,recall,auc))
+print(datetime.datetime.fromtimestamp(time()-times).strftime("%M:%S:%f"))
+
+from sklearn.metrics import roc_curve as ROC
+import matplotlib.pyplot as plt
+
+FPR, Recall, thresholds = ROC(Ytest,clf.decision_function(Xtest),pos_label=1)
+
+area = roc_auc_score(Ytest,clf.decision_function(Xtest))
+
+plt.figure()
+plt.plot(FPR, Recall, color='red',
+         label='ROC curve (area = %0.2f)' % area)
+plt.plot([0, 1], [0, 1], color='black', linestyle='--')
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('Recall')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
+
+maxindex = (Recall - FPR).tolist().index(max(Recall - FPR))
+
+thresholds[maxindex]
+
+
+from sklearn.metrics import accuracy_score as AC
+
+clf = SVC(kernel = "linear",C=3.1663157894736838,cache_size = 5000
+          ,class_weight = "balanced"
+         ).fit(Xtrain, Ytrain)
+
+prob = pd.DataFrame(clf.decision_function(Xtest))
+
+prob.loc[prob.iloc[:,0] >= thresholds[maxindex],"y_pred"]=1
+prob.loc[prob.iloc[:,0] < thresholds[maxindex],"y_pred"]=0
+
+times = time()
+score = AC(Ytest,prob.loc[:,"y_pred"].values)
+recall = recall_score(Ytest, prob.loc[:,"y_pred"])
+print("testing accuracy %f,recall is %f" % (score,recall))
+print(datetime.datetime.fromtimestamp(time()-times).strftime("%M:%S:%f"))
